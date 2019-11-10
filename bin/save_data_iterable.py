740e305739639d4bc93816a79540261a2547e066
Andrew Tritt <ajtritt@lbl.gov>
2019-11-10 07:11:51 -0800

    add argparse

--- bin/save_data_iterable.py
+++ bin/save_data_iterable.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import argparse
 
 from hdmf.common import get_hdf5io
 from hdmf.data_utils import DataChunkIterator
@@ -7,25 +8,28 @@ from hdmf.data_utils import DataChunkIterator
 from exabiome.sequence.convert import SeqIterator
 from exabiome.sequence.dna_table import DNATable
 
+parser = argparse.ArgumentParser()
+parser.add_argument('fof', type=str, help='file of Fasta files')
+parser.add_argument('out', type=str, help='output HDF5')
 
-fnapaths = [
-    "../deep_index/gtdb/test_data/genomes/all/GCA/000/989/525/GCA_000989525.1_ASM98952v1/GCA_000989525.1_ASM98952v1_cds_from_genomic.fna.gz",
-    "../deep_index/gtdb/test_data/genomes/all/GCA/000/435/995/GCA_000435995.1_MGS137/GCA_000435995.1_MGS137_cds_from_genomic.fna.gz"
-]
+args = parser.parse_args()
 
+with open(args.fof, 'r') as f:
+    fnapaths = [l[:-1] for l in f.readlines()]
 
-h5path =  "seq.h5"
+h5path =  args.out
 
 print("reading %d Fasta files" % len(fnapaths))
 print("Total size:", sum(os.path.getsize(f) for f in fnapaths))
 
 
-seqit = SeqIterator(fnapaths)
+seqit = SeqIterator(fnapaths, verbose=True)
 
 packed = DataChunkIterator.from_iterable(iter(seqit), maxshape=(None,), buffer_size=2**15, dtype=np.dtype('uint8'))
-seqindex = DataChunkIterator.from_iterable(seqit.index_iter, maxshape=(None,), buffer_size=2**10, dtype=np.dtype('int'))
-names = DataChunkIterator.from_iterable(seqit.names_iter, maxshape=(None,), buffer_size=2**10, dtype=np.dtype('U'))
-ids = DataChunkIterator.from_iterable(seqit.id_iter, maxshape=(None,), buffer_size=2**10, dtype=np.dtype('int'))
+seqindex = DataChunkIterator.from_iterable(seqit.index_iter, maxshape=(None,), buffer_size=2**0, dtype=np.dtype('int'))
+names = DataChunkIterator.from_iterable(seqit.names_iter, maxshape=(None,), buffer_size=2**0, dtype=np.dtype('U'))
+ids = DataChunkIterator.from_iterable(seqit.id_iter, maxshape=(None,), buffer_size=2**0, dtype=np.dtype('int'))
+taxa = DataChunkIterator.from_iterable(seqit.taxon_iter, maxshape=(None,), buffer_size=2**0, dtype=np.dtype('uint16'))
 
 
 with get_hdf5io(h5path, 'w') as io:
@@ -33,6 +37,7 @@ with get_hdf5io(h5path, 'w') as io:
                      io.set_dataio(names,    compression='gzip', chunks=(2**15,)),
                      io.set_dataio(packed,   compression='gzip', maxshape=(None,), chunks=(2**15,)),
                      io.set_dataio(seqindex, compression='gzip', maxshape=(None,), chunks=(2**15,)),
+                     io.set_dataio(taxa, compression='gzip', maxshape=(None,), chunks=(2**15,)),
                      id=io.set_dataio(ids, compression='gzip', maxshape=(None,), chunks=(2**15,)))
     io.write(table, exhaust_dci=False)
 
