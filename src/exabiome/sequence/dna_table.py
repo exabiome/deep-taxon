a036916e5466281470bb0363bbf6eb69eac12e51
Andrew Tritt <ajtritt@lbl.gov>
2019-11-19 15:37:20 -0800

    add read capability

--- src/exabiome/sequence/dna_table.py
+++ src/exabiome/sequence/dna_table.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 
 from hdmf.common import VectorIndex, VectorData, DynamicTable,\
-                        register_class, load_namespaces
+                        DynamicTableRegion, register_class, load_namespaces
 from hdmf.utils import docval, call_docval_func, get_docval, popargs
 from hdmf import Container
 
@@ -32,17 +32,24 @@ class BitpackedIndex(VectorIndex):
 class DNATable(DynamicTable):
 
     @docval(*get_docval(DynamicTable.__init__),
-            {'name': 'names', 'type': ('array_data', 'data'), 'doc': 'sequence names'},
-            {'name': 'sequence', 'type': ('array_data', 'data'), 'doc': 'bitpacked DNA sequence'},
-            {'name': 'index', 'type': ('array_data', 'data'), 'doc': 'index for sequence'},
-            {'name': 'taxon', 'type': ('array_data', 'data'), 'doc': 'index for sequence'})
+            {'name': 'names', 'type': ('array_data', 'data', VectorData), 'doc': 'sequence names'},
+            {'name': 'sequence', 'type': ('array_data', 'data', VectorData), 'doc': 'bitpacked DNA sequence'},
+            {'name': 'sequence_index', 'type': ('array_data', 'data', BitpackedIndex), 'doc': 'index for sequence'},
+            {'name': 'taxon', 'type': ('array_data', 'data', VectorData), 'doc': 'index for sequence'},
+            {'name': 'taxon_table', 'type': DynamicTable, 'doc': "target for 'taxon'", 'default': None})
     def __init__(self, **kwargs):
-        names, index, sequence, taxon = popargs('names', 'index', 'sequence', 'taxon', kwargs)
+        names, index, sequence, taxon, taxon_table = popargs('names', 'sequence_index', 'sequence', 'taxon', 'taxon_table', kwargs)
         columns = list()
-        columns.append(VectorData('names', 'sequence names', data=names))
-        columns.append(VectorData('sequence', 'bitpacked DNA sequences', data=sequence))
-        columns.append(BitpackedIndex('sequence_index', index, columns[-1]))
-        columns.append(VectorData('taxon', 'taxa for each sequence', data=taxon))
+        if isinstance(names, VectorData):      # data is being read -- here we assume that everything is a VectorData
+            columns.append(names)
+            columns.append(index)
+            columns.append(sequence)
+            columns.append(taxon)
+        else:
+            columns.append(VectorData('names', 'sequence names', data=names))
+            columns.append(VectorData('sequence', 'bitpacked DNA sequences', data=sequence))
+            columns.append(BitpackedIndex('sequence_index', index, columns[-1]))
+            columns.append(DynamicTableRegion('taxon', taxon, 'taxa for each sequence', taxon_table))
         kwargs['columns'] = columns
         call_docval_func(super().__init__, kwargs)
 
@@ -51,20 +58,25 @@ class DNATable(DynamicTable):
 class TaxaTable(DynamicTable):
 
     @docval(*get_docval(DynamicTable.__init__),
-            {'name': 'taxon_id', 'type': ('array_data', 'data'), 'doc': 'the taxon ID'},
-            {'name': 'embedding', 'type': ('array_data', 'data'), 'doc': 'the embedding for each taxon'})
+            {'name': 'taxon_id', 'type': ('array_data', 'data', VectorData), 'doc': 'the taxon ID'},
+            {'name': 'embedding', 'type': ('array_data', 'data', VectorData), 'doc': 'the embedding for each taxon'})
     def __init__(self, **kwargs):
         taxon_id, embedding = popargs('taxon_id', 'embedding', kwargs)
         columns = list()
-        columns.append(VectorData('taxon_id', 'taxonomy IDs from NCBI', data=taxon_id))
-        columns.append(VectorData('embedding', 'an embedding for each species', data=embedding))
+        if isinstance(taxon_id, VectorData):      # data is being read -- here we assume that everything is a VectorData
+            columns.append(taxon_id)
+            columns.append(embedding)
+        else:
+            columns.append(VectorData('taxon_id', 'taxonomy IDs from NCBI', data=taxon_id))
+            columns.append(VectorData('embedding', 'an embedding for each species', data=embedding))
         kwargs['columns'] = columns
         call_docval_func(super().__init__, kwargs)
 
 @register_class('DeepIndexFile', NS)
 class DeepIndexFile(Container):
 
-    __fields__ = ('dna_table', 'taxa_table')
+    __fields__ = ({'name': 'dna_table', 'child': True},
+                  {'name': 'taxa_table', 'child': True})
 
     @docval({'name': 'dna_table', 'type': DNATable, 'doc': 'the table storing DNA sequences'},
             {'name': 'taxa_table', 'type': TaxaTable, 'doc': 'the table storing taxa information'})
@@ -72,3 +84,9 @@ class DeepIndexFile(Container):
         dna_table, taxa_table = popargs('dna_table', 'taxa_table', kwargs)
         call_docval_func(super().__init__, {'name': 'root'})
         self.dna_table, self.taxa_table = dna_table, taxa_table
+
+    def __getitem__(self, i):
+        (name, seq, (taxon_name, taxon_emb)) = self.dna_table[i]
+
+    def __len__(self):
+        return len(self.dna_table)
