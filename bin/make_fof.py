683876a0f0ad852814a689345137d76a310d6279
Andrew Tritt <ajtritt@lbl.gov>
2019-12-06 13:43:59 -0800

    check taxon ID

--- bin/make_fof.py
+++ bin/make_fof.py
@@ -33,13 +33,21 @@ tree = TreeNode.read(args.tree, format='newick')
 logger.info('getting taxa IDs from tree')
 tid_set = set()
 for tip in tree.tips():
-    tid_set.add(tip.name[3:].replace(' ', '_'))
+    name = tip.name
+    if name[0:2] in ('RS', 'GB'):
+        name = name[3:]
+    name = name.replace(' ', '_')
+    tid_set.add(name)
 
 
 logger.info('getting Fasta paths from %s' % args.fadir)
 suffix = 'protein' if args.protein else 'cds_from_genomic'
 pattern = "%s/all/G*/[0-9][0-9][0-9]/[0-9][0-9][0-9]/[0-9][0-9][0-9]/*/*%s*.gz" % (args.fadir, suffix)
-for path in glob.glob(pattern):
+for path in glob.iglob(pattern):
     taxa_id = get_taxa_id(path)
     if taxa_id in tid_set:
         print(path, file=sys.stdout)
+        tid_set.remove(taxa_id)
+
+for tid in sorted(tid_set):
+    print(tid, file=sys.stderr)
