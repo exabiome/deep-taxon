d6a3170aff3524f96dfa85cb2dfc791c6764b714
Andrew Tritt <ajtritt@lbl.gov>
2020-01-13 14:14:19 -0800

    add ability to load and profile

--- src/exabiome/nn/train.py
+++ src/exabiome/nn/train.py
@@ -39,6 +39,7 @@ def parse_logger(string):
     else:
         ret = logging.getLogger(string)
         hdlr = logging.FileHandler(string)
+    ret.setLevel(logging.INFO)
     ret.addHandler(hdlr)
     hdlr.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
     return ret
@@ -58,7 +59,7 @@ def parse_args(desc, argv=None):
     parser.add_argument('input', type=str, help='the HDF5 DeepIndex file')
     parser.add_argument('output', type=str, help='file to save model', default=None)
     parser.add_argument('-c', '--checkpoint', type=str, help='resume training from file', default=None)
-    parser.add_argument('-r', '--resume', type=str, help='resume training from checkpoint stored in output', default=None)
+    parser.add_argument('-r', '--resume', action='store_true', help='resume training from checkpoint stored in output', default=False)
     parser.add_argument('-b', '--batch_size', type=int, help='batch size', default=64)
     parser.add_argument('-e', '--epochs', type=int, help='number of epochs to use', default=1)
     parser.add_argument('-p', '--protein', action='store_true', default=False, help='input contains protein sequences')
@@ -68,6 +69,8 @@ def parse_args(desc, argv=None):
     parser.add_argument('-t', '--test_size', type=parse_test_size, default=0.2, help='size of test split')
     parser.add_argument('-d', '--debug', action='store_true', default=False, help='run in debug mode i.e. only run two batches')
     parser.add_argument('-l', '--logger', type=parse_logger, default='', help='path to logger [stdout]')
+    parser.add_argument('--prof', type=str, default=None, metavar='PATH', help='profile training loop dump results to PATH')
+    parser.add_argument('-L', '--load', action='store_true', default=False, help='load data into memory before running training loop')
 
     if len(argv) == 0:
         parser.print_help()
@@ -78,7 +81,9 @@ def parse_args(desc, argv=None):
     if args.checkpoint is None:
         ret['checkpoint'] = False
     if args.resume:
-        ret['checkpoint'] = True
+        if not isinstance(ret['checkpoint'], str):
+            # don't overwrite a given path
+            ret['checkpoint'] = True
     ret.pop('resume')
     return ret
 
@@ -117,11 +122,17 @@ def parse_args(desc, argv=None):
         {'name': 'checkpoint', 'type': (str, bool), 'default': False,
          'help': 'If True, resume from checkpoint in output. If string, assume string is path to checkpoint file'},
 
+        {'name': 'load', 'type': bool, 'default': False,
+         'help': 'load data into memory before training loop'},
+
         {'name': 'logger', 'type': logging.Logger, 'default': None,
          'help': 'the path to the log file to use'},
 
         {'name': 'debug', 'type': bool, 'default': False,
          'help': 'run in debug mode (one batch per epoch)'},
+
+        {'name': 'prof', 'type': str, 'default': None,
+         'help': 'profile training loop and dump results to given path'},
 
         {'name': '', 'type': None, 'help': '', 'default': None},
         is_method=False, allow_extra=True)
@@ -141,14 +152,17 @@ def run_serial(**kwargs):
     batch_size = kwargs['batch_size']
     test_size = kwargs['test_size']
     cuda_index = kwargs['cuda_index']
+    load = kwargs['load']
 
     logger = kwargs['logger']
     debug = kwargs['debug']
+    prof = kwargs['prof']
 
 
     # set up logger
     if logger is None:
         logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
 
     if debug:
         logger.setLevel(logging.DEBUG)
@@ -167,17 +181,21 @@ def run_serial(**kwargs):
 
     train_loss = np.zeros(epochs)
     test_loss = np.zeros(epochs)
+    best_epoch = 0         # the epoch of the best model state
+    best_state = None      # the best model state i.e. state that achieves lowest test loss
     if checkpoint:
         logger.info('picking up from checkpoint %s' % checkpoint)
         checkpoint = torch.load(checkpoint)
-        model.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        curr_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['model_state'])
+        optimizer.load_state_dict(checkpoint['optimizer_state'])
+        curr_epoch = checkpoint['epoch'] + 1
         train_loss = np.append(checkpoint['train_loss'], np.zeros(epochs))
         test_loss = np.append(checkpoint['test_loss'], np.zeros(epochs))
         test_size = checkpoint['test_size']
         split_seed = checkpoint['split_seed']
         batch_size = checkpoint['batch_size']
+        best_epoch = checkpoint['best_epoch']
+        best_state = checkpoint['best_state']
 
     last_epoch = curr_epoch + epochs
     criterion = nn.MSELoss()
@@ -190,12 +208,18 @@ def run_serial(**kwargs):
                                                    test_size=test_size,
                                                    batch_size=batch_size,
                                                    device=device,
+                                                   load=load,
                                                    random_state=split_seed)
 
     logger.info(f'starting with epoch {curr_epoch+1}')
     logger.info(f'saving results to {output}')
 
-    nprint = len(train_loader.dataset) // 10
+    nprint = (len(train_loader.dataset) / batch_size) // 10
+
+    if prof:
+        import cProfile
+        pr = cProfile.Profile()
+        pr.enable()
 
     before = datetime.now()
     for curr_epoch in range(curr_epoch, last_epoch):  # loop over the dataset multiple times
@@ -217,7 +241,6 @@ def run_serial(**kwargs):
             outputs = model(seqs, orig_len=orig_lens)
 
             logger.debug('criterion')
-            #emb = emb.to(device)
             loss = criterion(outputs, emb)
             logger.debug('backward')
             loss.backward()
@@ -236,26 +259,37 @@ def run_serial(**kwargs):
         logger.debug('test loss')
         for i, data in enumerate(test_loader, 0):
             idx, seqs, emb, orig_lens = data
-            #emb = emb.to(device)
             outputs = model(seqs, orig_len=orig_lens)
             crit = criterion(outputs, emb).item()
             test_loss[curr_epoch] += crit
             if debug:
                 break
 
+        if test_loss[curr_epoch] <= test_loss[best_epoch]:
+            logger.info(f'updating best state')
+            logger.info(f'- previous best loss: {test_loss[best_epoch]} (epoch {best_epoch+1})')
+            logger.info(f'- current loss:       {test_loss[curr_epoch]}')
+            best_epoch = curr_epoch
+            best_state = model.state_dict()
+
         logger.debug('checkpointing')
-        torch.save({'epoch': last_epoch,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict(),
+        torch.save({'epoch': curr_epoch,
+                    'model_state': model.state_dict(),
+                    'optimizer_state': optimizer.state_dict(),
                     'train_loss': train_loss,
-                    'test_loss': train_loss,
+                    'test_loss': test_loss,
+                    'best_epoch': best_epoch,
+                    'best_state': best_state,
                     'split_seed': split_seed,
                     'test_size': test_size,
                     'batch_size': batch_size,
-                    'loss': loss,
                     }, output)
 
         logger.info(f'epoch {curr_epoch+1} complete')
+
+    if prof:
+        pr.disable()
+        pr.dump_stats(prof)
 
     after = datetime.now()
     logger.info('Finished Training. Took %s seconds' % (after-before).total_seconds())
