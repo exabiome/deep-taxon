d7cc0ade95ae9517cae37d87792a7270d42a5095
Andrew Tritt <ajtritt@lbl.gov>
2020-01-17 17:33:39 -0800

    make module executable

--- src/exabiome/nn/spp_cnn.py
+++ src/exabiome/nn/spp_cnn.py
@@ -138,3 +138,22 @@ class GroupPool(nn.Module):
 
     def forward(self, x, group=None):
         return self._pool_groups(x, group)
+
+if __name__ == '__main__':
+    from .train import parse_args, run_serial
+    import torch.optim as optim
+
+    args = parse_args("Train CNN with Spatial Pyramidal Pooling",
+                      [['-E', '--emb_nc'], dict(type=int, default=0, help='the number of embedding channels. default is no embedding')])
+
+    input_nc = 4
+    if args['protein']:
+        input_nc = 26
+
+    model = SPP_CNN(input_nc, 64, kernel_size=7, emb_nc=args['emb_nc'])
+    optimizer = optim.Adam(model.parameters(), lr=args['lr'])
+
+    if args['emb_nc'] > 0:
+        args['ohe'] = False
+
+    run_serial(model=model, optimizer=optimizer, **args)
