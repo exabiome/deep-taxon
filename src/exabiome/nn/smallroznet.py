cb7f4142b5cebe741bbcf95707c271a9adb892d4
Andrew Tritt <ajtritt@lbl.gov>
2020-04-22 17:50:43 -0700

    add smaller version of roznet

---
+++ src/exabiome/nn/smallroznet.py
@@ -0,0 +1,90 @@
+import math
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+
+class SmallRozNet(nn.Module):
+    '''
+    A 1D CNN with 5 convolutional layers, followed by 3 fully-connected layers
+
+    Args:
+        input_nc (int):  the input number of channels
+    '''
+
+    def __init__(self, input_nc, n_outputs=2, first_kernel_size=7, maxpool=True):
+        super(SmallRozNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv1d(input_nc, 32, kernel_size=first_kernel_size, stride=1, padding=2),
+            nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=3, stride=1),
+            nn.Conv1d(32, 64, kernel_size=5, padding=2),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=3, stride=1),
+            nn.Conv1d(64, 96, kernel_size=3, padding=1),
+            nn.BatchNorm1d(96),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(96, 96, kernel_size=3, padding=1),
+            nn.BatchNorm1d(96),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(96, 64, kernel_size=3, padding=1),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=3, stride=1),
+        )
+        if maxpool:
+            self.pool = nn.AdaptiveMaxPool1d(12)
+        else:
+            self.pool = nn.AdaptiveAvgPool1d(12)
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(64*12, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(256, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, n_outputs),
+            #nn.BatchNorm1d(n_outputs)
+        )
+
+    def forward(self, x, **kwargs):
+        x = self.features(x)
+        x = self.pool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+
+if __name__ == '__main__':
+    from .train import parse_args, run, check_model, load_dataset
+
+    args = parse_args("Train CNN with Spatial Pyramidal Pooling")
+                      #[['-E', '--emb_nc'], dict(type=int, default=0, help='the number of embedding channels. default is no embedding')])
+
+    input_nc = 4
+    if args['protein']:
+        input_nc = 26
+
+    if args['sanity']:
+        input_nc = 5
+
+
+    dataset, io = load_dataset(path=args['input'], **args)
+
+    if args['classify']:
+        n_outputs = len(dataset.difile.taxa_table)
+    else:
+        n_outputs = dataset.difile.n_emb_components
+
+    model = check_model(SmallRozNet(input_nc, n_outputs=n_outputs), **args)
+
+    args['pad'] = True
+
+    run(dataset, model, **args)
+
+    io.close()
