cf3d0a2cd23476884ce0a4646a30e046c3ae33bf
Andrew Tritt <ajtritt@lbl.gov>
2020-06-18 17:22:07 -0700

    remove unnecessary line

--- src/exabiome/nn/loss.py
+++ src/exabiome/nn/loss.py
@@ -6,7 +6,6 @@ class DistMSELoss(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.mse = nn.MSELoss()
 
     def forward(self, output, target):
         x2 = output.pow(2).sum(axis=1)
