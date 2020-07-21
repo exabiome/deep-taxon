297622c542c88010cf116ccc942932a8bcb19bca
Andrew Tritt <ajtritt@lbl.gov>
2020-07-21 12:25:13 -0700

    Add scripts for running on Cori GPUs (#17)
    * add script for running on Cori-GPUs
    
    * add command for showing CUDA availability
    
    * add extra return value
    
    * add a script for building environment
    
    * clean up job script

---
+++ scripts/train_cgpu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --constraint=gpu
+#SBATCH --nodes=1
+#SBATCH --time=4:00:00
+#SBATCH --cpus-per-task=10
+#SBATCH --gpus=4
+# SBATCH --account=m2865
+# SBATCH --ntasks-per-node=8
+# SBATCH --gpus-per-task=1
+# SBATCH --exclusive
+#SBATCH -J train-cgpu
+#SBATCH -o /global/homes/a/ajtritt/projects/exabiome/logs/%x-%j.out
+
+# Setup software
+module load python
+conda activate exabiome_38
+
+DI_DIR=$CSCRATCH/exabiome/deep-index/
+LOSS=M
+MODEL=roznet
+EXP=g4_b32_lr0.001
+OUTDIR=$DI_DIR/train/datasets/medium/chunks/$MODEL/$LOSS
+
+# Run the training
+srun -u deep-index train \
+            $MODEL $DI_DIR/input/ar122_r89.genomic.small.deep_index.input.h5 $OUTDIR \
+            -$LOSS -g 4 -b 32 -s 3001 -S 1000 -W 1000 --lr 0.001 -e 10 -E $EXP \
+            > $OUTDIR/logs/$EXP.log 2>&1
