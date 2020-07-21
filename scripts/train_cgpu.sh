bf9e393941dea173fc67ef65cfc34bcd29f617fa
Andrew Tritt <ajtritt@lbl.gov>
2020-07-21 13:39:02 -0700

    add variables for flags

--- scripts/train_cgpu.sh
+++ scripts/train_cgpu.sh
@@ -15,14 +15,20 @@
 module load python
 conda activate exabiome_38
 
+G=4
+B=32
+LR=0.001
+EXP=g${G}_b${B}_lr${LR}
+
 DI_DIR=$CSCRATCH/exabiome/deep-index/
 LOSS=M
 MODEL=roznet
-EXP=g4_b32_lr0.001
-OUTDIR=$DI_DIR/train/datasets/medium/chunks/$MODEL/$LOSS
+DATASET=medium
+OUTDIR=$DI_DIR/train/datasets/$DATASET/chunks/$MODEL/$LOSS
+mkdir -p $OUTDIR/logs
 
 # Run the training
 srun -u deep-index train \
-            $MODEL $DI_DIR/input/ar122_r89.genomic.small.deep_index.input.h5 $OUTDIR \
-            -$LOSS -g 4 -b 32 -s 3001 -S 1000 -W 1000 --lr 0.001 -e 10 -E $EXP \
+            $MODEL $DI_DIR/input/ar122_r89.genomic.$DATASET.deep_index.input.h5 $OUTDIR \
+            -$LOSS -g $G -b $B -s 3001 -S 1000 -W 1000 --lr $LR -e 50 -E $EXP \
             > $OUTDIR/logs/$EXP.log 2>&1
