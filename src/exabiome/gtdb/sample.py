7f7c143f1b37183aee2765b1e24abd8800b9b38d
Andrew Tritt <ajtritt@lbl.gov>
2020-08-24 17:38:25 -0700

    fix typo

--- src/exabiome/gtdb/sample.py
+++ src/exabiome/gtdb/sample.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def sample_taxa(taxa_df, n_classes=1, n_genera=1, n_species=1):
+def sample_taxa(taxdf, n_classes=1, n_genera=1, n_species=1):
     """
     Sample taxa from the given taxonomy data frame.
 
