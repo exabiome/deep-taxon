b6136958bf8aaeb92d47457eb5ecab518116ad4f
Ali Zaidi <azaidi06@gmail.com>
2021-07-26 07:38:54 -0700

    new effnet instantiation (b0-b7), looks good but not training correctly

--- src/exabiome/nn/models/effnet/model.py
+++ src/exabiome/nn/models/effnet/model.py
@@ -5,51 +5,44 @@ import os
 import h5py
 import torch.nn as nn
 import pdb
+import json
 
 from .. import model, AbstractLit
 
 
 __all__ = ['DepSepBlock', 'InvertedResidualBlock', 'SqueezeExcite',
-          'EffNet_b0',]# 'p_list']
+          'EffNet_b0',]
 
-p_list = [
-        [[16,96,3,24,3,2,1], [24,144,6,24,3,1,1]], #layer 1
-        [[24,144,6,40,5,2,2], [40,240,10,40,5,1,2]], #layer 2 
-        [[40,240,10,80,3,2,1],[80,480,20,80,3,1,1], 
-                                [80,480,20,80,3,1,1]], #layer 3
-        [[80,480,20,112,5,1,2], [112,672,28,112,5,1,2], 
-                                 [112,672,28,112,5,1,2]], #layer 4
-        [[112,672,28,192,5,2,2],[192,1152,48,192,5,1,2],
-         [192,1152,48,192,5,1,2], [192,1152,48,192,5,1,2]], #layer 5
-        [[192,1152,48,320,3,2,1]] #layer 6
-         ]
-
-
-
-class DepSepBlock(nn.Module):
-    def __init__(self):
-        super(DepSepBlock, self).__init__()
-        self.conv1 = nn.Conv1d(32, 32, 3, bias=False, groups=32)
-        self.bn1 = nn.BatchNorm1d(32)
-        self.act = nn.SiLU(inplace=True)
-        self.conv2 = nn.Conv1d(32, 8, 1)
-        self.conv3 = nn.Conv1d(8, 32, 1)
-        self.conv4 = nn.Conv1d(32, 16, 1, stride=1, bias=False)
-        self.bn2 = nn.BatchNorm1d(16)
-    
+class SqueezeExcite(nn.Module):
+    def __init__(self, ch_in, mid_ch):
+        super().__init__()
+        self.layer = nn.Sequential(
+            nn.Conv1d(ch_in, mid_ch, kernel_size=1, stride=1),
+            nn.SiLU(inplace=True),
+            nn.Conv1d(mid_ch, ch_in, kernel_size=1, stride=1)
+        )
     def forward(self, x):
-        identity = x
-        
+        out = self.layer(x)
+        return out + x
+
+    
+class DepthwiseSeperableBlock(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(DepthwiseSeperableBlock, self).__init__()
+        self.conv1 = nn.Conv1d(in_ch, in_ch, 3, bias=False, groups=in_ch)
+        self.bn1 = nn.BatchNorm1d(in_ch)
+        self.act = nn.SiLU(inplace=True)
+        self.se = SqueezeExcite(in_ch, int(in_ch/4))
+        self.conv2 = nn.Conv1d(in_ch, out_ch, 1, stride=1, bias=False)
+        self.bn2 = nn.BatchNorm1d(out_ch)
+    
+    def forward(self, x):        
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.act(out)
+        out = self.se(out)
         out = self.conv2(out)
-        out = self.act(out)
-        out = self.conv3(out)
-        out = self.conv4(out)
         out = self.bn2(out)
-        #out = nn.Identity(out)
-        
         return out
 
     
@@ -67,12 +60,12 @@ class InvertedResidualBlock(nn.Module):
         self.squeeze = SqueezeExcite(mid_ch, sq_ch)
         self.conv5 = nn.Conv1d(mid_ch, out_ch, kernel_size=1, stride=1, bias=False) 
         self.bn3 = nn.BatchNorm1d(out_ch)
-        
-        #self.downsample = nn.Conv1d(in_ch, out_ch,1, stride=stride)
-        
+        self.downsample = nn.Conv1d(in_ch, out_ch,1, stride=stride)
+
+
+                
     def forward(self, x):
-        identity = x
-        
+        #identity = x
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.act(out)
@@ -81,97 +74,80 @@ class InvertedResidualBlock(nn.Module):
         out = self.squeeze(out)
         out = self.conv5(out)
         out = self.bn3(out)
-        
-        #identity = self.downsample(x)
-        #out += identity
+        identity = self.downsample(x)
+        out += identity
         out = self.act(out)
-        
+
         return out
 
     
-class SqueezeExcite(nn.Module):
-    def __init__(self, ch_in, mid_ch):
-        super().__init__()
-        self.layer = nn.Sequential(
-            nn.Conv1d(ch_in, mid_ch, kernel_size=1, stride=1),
-            nn.SiLU(inplace=True),
-            nn.Conv1d(mid_ch, ch_in, kernel_size=1, stride=1)
-        )
-    def forward(self, x):
-        out = self.layer(x)
-        return out + x
-    
-#@model("effnet-b0")    
-class EffNet(AbstractLit):
-#class EffNet_b0(nn.Module):
-    def __init__(self, hparams,#param_list=p_list, 
-                ):#avg_out=200, out_feats=512, n_classes=18):
-        #super(EffNet_b0, self).__init__()
-        super(EffNet, self).__init__(hparams)
-        
-        param_list = [
-        [[16,96,3,24,3,2,1], [24,144,6,24,3,1,1]], #layer 1
-        [[24,144,6,40,5,2,2], [40,240,10,40,5,1,2]], #layer 2 
-        [[40,240,10,80,3,2,1],[80,480,20,80,3,1,1], 
-                                [80,480,20,80,3,1,1]], #layer 3
-        [[80,480,20,112,5,1,2], [112,672,28,112,5,1,2], 
-                                 [112,672,28,112,5,1,2]], #layer 4
-        [[112,672,28,192,5,2,2],[192,1152,48,192,5,1,2],
-         [192,1152,48,192,5,1,2], [192,1152,48,192,5,1,2]], #layer 5
-        [[192,1152,48,320,3,2,1]] #layer 6
-         ]
-        self.embedding = nn.Embedding(20, 8)
-        self.avg_out = 20#avg_out
-        self.out_feats = 1#out_feats
-        self.n_classes = 20#n_classes
-        self.param_list = param_list
+def make_dep_sep(in_ch, out_ch, num_layers):
+    ll = [DepthwiseSeperableBlock(in_ch, out_ch)]
+    for _ in range(1, num_layers):
+        ll.append(DepthwiseSeperableBlock(out_ch, out_ch))
+    return nn.Sequential(*ll)   
 
-        self.conv1 = nn.Conv1d(8, 32, kernel_size=3, stride=2, padding=1)
-        self.bn1 = nn.BatchNorm1d(32)
-        self.silu = nn.SiLU(inplace=True)
-        self.layer0 = DepSepBlock()
-        self.layer_list = []
-        for x in range(len(self.param_list)):
-            self.layer_list.append(self._make_layer(self.param_list[x]))
-        self.layer_list = nn.Sequential(*self.layer_list)
-        self.conv2 = nn.Conv1d(320, self.n_classes, 1, 1)
-        self.bn2 = nn.BatchNorm1d(self.n_classes)
-        self.avgpool = nn.AdaptiveAvgPool1d(output_size=self.avg_out)
-        self.fc = nn.Linear(in_features=self.avg_out, 
-                            out_features=self.out_feats)
+
+def make_inv_res(in_ch, mid_ch, sq_ch, out_ch,ks, stride, padding, num_layers):
+    ll = [InvertedResidualBlock(in_ch, mid_ch[0], sq_ch[0], 
+                                out_ch,ks, stride, padding)]
+    for _ in range(1, num_layers):
+        ll.append(InvertedResidualBlock(out_ch, mid_ch[1], sq_ch[1], out_ch,
+                                        ks, stride, padding))
+    return nn.Sequential(*ll)
+
+
+def make_inv_res_block(inverted_residual_params):
+    inv_res = [make_inv_res(*x) for x in inverted_residual_params]
+    return nn.Sequential(*inv_res)
+      
+def make_base(in_ch, out_ch):
+    return nn.Sequential(
+        nn.Conv1d(1, out_ch, kernel_size=3, stride=2, padding=1, bias=False),
+        nn.BatchNorm1d(out_ch),
+        nn.SiLU(inplace=True))
+
+def make_head_layer(head_params, out_classes=20):
+    in_ch, out_ch = head_params
+    return nn.Sequential(nn.Conv1d(in_ch, 20, kernel_size=1, stride=1),
+                         nn.BatchNorm1d(20),
+                         nn.SiLU(inplace=True),
+                         nn.AdaptiveAvgPool1d(output_size=20),
+                         nn.Linear(20, 1))#out_classes))
+
+def get_params():
+    f = open('params.json')
+    p_list = json.load(f)
+    f.close()
+    return p_list
+    
+    
+class EffNet(AbstractLit):
+    def __init__(self, hparams, model_name):
+        super(EffNet, self).__init__(hparams)
+        self.param_list = get_params()[model_name]
+
+        self.base = make_base(*self.param_list['base_params'])
+        self.dep_sep = make_dep_sep(*self.param_list['dep_sep_params'])
+        self.inv_res = make_inv_res_block(self.param_list['inv_res_params'])
+        self.head = make_head_layer(self.param_list['head_params'])
         
         for m in self.modules():
             if isinstance(m, nn.Conv1d):
-                #nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
                 fan_out = m.kernel_size[0] * m.out_channels
                 nn.init.normal_(m.weight, mean=0, std=(np.sqrt(2/fan_out)))
-                #.kernel_size[0] * m.conv1.out_channels
             elif isinstance(m, (nn.BatchNorm1d, nn.GroupNorm)):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-    
-    def _make_layer(self, param_list):
-        layers = []
-        for x in range(len(param_list)):
-            layers.append(InvertedResidualBlock(*param_list[x]))
-        return nn.Sequential(*layers)
+
         
     def _forward_impl(self, x):
         #pdb.set_trace()
-        #x = x.unsqueeze(1).to(torch.half)
-        x = self.embedding(x)
-        x = x.permute(0,2,1)
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.silu(x)
-        
-        x = self.layer0(x)
-        x = self.layer_list(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.avgpool(x)
-        x = self.fc(x)
-        
+        x = x.unsqueeze(1).to(torch.half)
+        x = self.base(x)
+        x = self.dep_sep(x)
+        x = self.inv_res(x)
+        x = self.head(x)
         x = x.squeeze()
         return x
     
@@ -179,9 +155,22 @@ class EffNet(AbstractLit):
         return self._forward_impl(x)
 
     
-    
 @model('effnet-b0')
 class EffNetB0(EffNet):
     def __init__(self, hparams):
-        super().__init__(hparams)
+        super().__init__(hparams, model_name='efficientnet_b0')
         
+@model('effnet-b1')
+class EffNetB0(EffNet):
+    def __init__(self, hparams):
+        super().__init__(hparams, model_name='efficientnet_b1')
+        
+@model('effnet-b2')
+class EffNetB0(EffNet):
+    def __init__(self, hparams):
+        super().__init__(hparams, model_name='efficientnet_b2')
+        
+@model('effnet-b3')
+class EffNetB0(EffNet):
+    def __init__(self, hparams):
+        super().__init__(hparams, model_name='efficientnet_b3')
