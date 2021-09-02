eae004616fd6913152a8a1f706c129acdd401378
Andrew Tritt <ajtritt@lbl.gov>
2021-09-02 17:02:49 -0400

    fix typo

--- bin/deep-taxon.py
+++ bin/deep-taxon.py
@@ -43,7 +43,7 @@ command_dict = {
     'show-hparams': Command('nn.train.show_hparams', 'Print hparams from a checkpoint file'),
     'to-onnx': Command('nn.deploy.to_onnx', 'convert checkpoint to ONNX format'),
     'agg-chunks': Command('nn.summarize.aggregate_chunks', 'aggregate sequence chunks to get NN outputs for individual sequences'),
-    'agg-seqs': Command('nn.summarize.aggregate_seqs', 'aggregate sequence chunks to get NN outputs for individual sequences'),
+    'agg-seqs': Command('nn.summarize.aggregate_seqs', 'aggregate sequence chunks to get NN outputs for individual taxons (i.e. labels)'),
     'tax-acc': Command('nn.summarize.taxonomic_accuracy', 'calculate classificaiton accuracy across all possible taxonomic levels'),
     'model-info': Command('nn.train.get_model_info', 'Construct and print info about model'),
     'fix-model': Command('nn.train.fix_model', 'Fix model trained with old embedding'),
