cfe8e57fc4710c5a5a2276a32ea751de501bb092
Andrew Tritt <ajtritt@lbl.gov>
2022-05-03 17:56:28 -0700

    remove dereferencing since we have already done that

--- src/exabiome/sequence/dna_table.py
+++ src/exabiome/sequence/dna_table.py
@@ -491,7 +491,8 @@ class DeepIndexFile(Container):
     def get(self, arg):
         arg = self.__translate_arg(arg)
         idx = self.seq_table.id[arg]
-        label = self.genome_table['rep_idx'].get(self.seq_table['genome'].get(arg, index=True), index=True)
+        #label = self.genome_table['rep_idx'].get(self.seq_table['genome'].get(arg, index=True), index=True)
+        label = arg
         label = self.labels[label]
         seq = self.seq_table['sequence'].get(arg, index=True)   # sequence data
         length = self.seq_table['length'].get(arg)
@@ -505,6 +506,9 @@ class DeepIndexFile(Container):
         return copy.copy(self.__indices)
 
     def __translate_arg(self, arg):
+        """
+        Translate from a subset index to the original sequence index
+        """
         if self.__indices is None:
             return arg
         else:
@@ -855,42 +859,3 @@ class RevCompFilter(DIFileFilter):
             raise ValueError("Cannot run without loading data. Use -l to load data") from e
         item['id'] = oarg if oarg >= 0 else len(self) + oarg
         return item
-
-
-class DIFileManager:
-
-    def __init__(self, path, tgt_tax_lvl, comm=None, load_data=False, rank=0, size=1):
-        self.path = path
-        self.comm = comm
-        self.load_data = load_data
-        self.difile = None
-        self._world_size = size
-        self._global_rank = rank
-        self.tgt_tax_lvl = tgt_tax_lvl
-
-    def open(self):
-        """Open the HDMF file and set up chunks and taxonomy label"""
-        if self.comm is not None:
-            self.io = get_hdf5io(self.path, 'r', comm=self.comm, driver='mpio')
-        else:
-            self.io = get_hdf5io(self.path, 'r')
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            self.difile = self.io.read()
-
-        if self._world_size > 1:
-            self.difile.set_sequence_subset(distsplit(len(self.difile), self._world_size, self._global_rank))
-
-        self.load(sequence=self.load_data)
-
-        self.difile.set_label_key(self.tgt_tax_lvl)
-
-    def load(self, sequence=False, device=None):
-        _load = lambda x: x[:]
-        self.difile.seq_table['id'].transform(_load)
-        self.difile.seq_table['length'].transform(lambda x: x[:].astype(int))
-        self.difile.seq_table['sequence_index'].transform(_load)
-        if sequence:
-            self.difile.seq_table['sequence_index'].target.transform(_load)
-
-
