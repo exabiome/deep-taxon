39189a0b166538191777d89d85dde4f542fd3b01
Andrew Tritt <ajtritt@lbl.gov>
2022-05-11 18:13:14 -0700

    protect executable

--- bin/deep-taxon.py
+++ bin/deep-taxon.py
@@ -4,4 +4,5 @@ path = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..', 'src'))
 sys.path.insert(0, path)
 
 from deep_taxon import main
-main()
+if __name__ == '__main__':
+    main()
