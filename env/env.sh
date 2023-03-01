f5fd663bcc30216b8da2e98c5e467f4d45130a1f
ajtritt <ajtritt@lbl.gov>
2023-03-01 14:41:29 -0800

    update environment build code to use new PyTorch

--- env/env.sh
+++ env/env.sh
@@ -1,6 +1,7 @@
 ENV_NAME=${1:?Please specify an environment name}
-conda create --yes -n $ENV_NAME pytorch torchvision torchaudio cudatoolkit=11.6 torchtext -c pytorch -c conda-forge
-if [ $? -ne 0 ]; then echo "Failed to create environment $ENV_NAME"; exit 1; fi
+module load python
+conda create --yes -n $ENV_NAME pytorch torchvision torchaudio pytorch-cuda=11.7 torchtext cython -c pytorch -c nvidia
+if [ $? -ne 0 ]; then echo "Failed to create environment $ENV_NAME"; return 1; fi
 conda activate $ENV_NAME
 MPICC="cc -target-accel=nvidia80 -shared" pip install --force-reinstall --no-cache-dir --no-binary=mpi4py mpi4py
 module load cray-hdf5-parallel
