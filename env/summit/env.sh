98b2d1e97695eb6bfe89f9e050697e176bfdcf4c
Andrew Tritt <ajtritt@lbl.gov>
2023-03-03 19:18:05 -0500

    Add pip install requirements

--- env/summit/env.sh
+++ env/summit/env.sh
@@ -1,5 +1,6 @@
 ENV_NAME=${1:?Please specify an environment name}
 
 module load open-ce-olcf/1.5.2-py39-0
-conda create --clone -n $ENV_NAME --clone open-ce-olcf-1.5.2-py39-0 --yes
-conda install -c conda-forge umap-learn -n $ENV_NAME
+conda create -n $ENV_NAME --clone open-ce-olcf-1.5.2-py39-0 --yes
+conda install -n $ENV_NAME -c conda-forge umap-learn --yes
+pip install -r requirements.txt
